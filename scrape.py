89eef640645cab586936ad7a262cdfa9266e74db
Avesh Singh <avesh.singh@raisingsuperstars.in>
2022-07-23 16:28:05 +0530

    added preprocessing steps to article sentences

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
+from curses.ascii import isalnum
 from os import walk
 import re
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import glob
 from infer_sent import InferSent
 import torch
@@ -9,6 +12,8 @@ import pickle
 import sys
 
 prepared_dir = "prepared"
+stopwords = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
 
 
 def read_articles(article_set="D0601A", data_dir="DUC2006_Summarization_Documents/duc2006_docs", write=True):
@@ -29,9 +34,18 @@ def read_articles(article_set="D0601A", data_dir="DUC2006_Summarization_Document
 
         all_paras = [para.replace("</P>", "") for para in paras[:-1]]
 
-        sentences = [sentence.replace("\n", " ").strip() for para in all_paras for sentence in sent_tokenize(para)]
-        if len(sentences) > 0:
-            article_sentences.append({"filename": article["filename"], "sentences": sentences})
+        sentences = [sentence.replace("\n", " ").strip(
+        ) for para in all_paras for sentence in sent_tokenize(para)]
+        new_sentences = []
+        for sentence in sentences:
+            sentence = re.sub(r"####", " ", re.sub(
+                r"\s+", "", re.sub(r" ", "####", sentence)))
+            filtered_words = [lemmatizer.lemmatize(
+                word) for word in word_tokenize(sentence) if word.isalnum() and word not in stopwords]
+            new_sentences.append(" ".join(filtered_words))
+        if len(new_sentences) > 0:
+            article_sentences.append(
+                {"filename": article["filename"], "sentences": new_sentences})
     if write:
         with open(f"{prepared_dir}/{article_set}_article_sentences.pkl", 'wb') as file:
             pickle.dump(article_sentences, file)
@@ -71,15 +85,16 @@ def calculate_similarities(article_set, article_sentences, summaries):
 
     article_sentence_embeddings = []
     for i, article in enumerate(article_sentences):
-        print(f"article {article['filename']}, # sentences {len(article['sentences'])}")
+        print(
+            f"article {article['filename']}, # sentences {len(article['sentences'])}")
         if len(article['sentences']) == 0:
             print("found 0")
         article_sentence_embeddings.append({"filename": article["filename"], "embeddings": [model.encode(article[
-                                                                                                           'sentences'])]})
+            'sentences'])]})
     summary_sentence_embeddings = model.encode(summaries)
     similarities = [[[[] for _ in range(len(summary_sentence_embeddings))] for _ in range(len(
         article_sentence_embeddings[i]["embeddings"]))] for i
-                    in range(len(article_sentence_embeddings))]
+        in range(len(article_sentence_embeddings))]
 
     for i, article in enumerate(article_sentence_embeddings):
         print(article["filename"])
@@ -100,53 +115,53 @@ if __name__ == "__main__":
             # "D0601A",
             # "D0603C",
             #     "D0605E",
-                # "D0607G",
-                # "D0609I",
-                # "D0611B",
-                # "D0613D",
-                # "D0615F",
-                # "D0617H",
-                # "D0619A",
-                # "D0621C",
-                # "D0623E",
-                # "D0625G",
-                # "D0627I",
-                # "D0629B",
-                # "D0631D",
-                # "D0633F",
-                # "D0635H",
-                # "D0637A",
-                # "D0639C",
-                # "D0641E",
-                # "D0643G",
-                # "D0645I",
-                "D0647B",
-                "D0649D",
-                "D0602B",
-                "D0604D",
-                "D0606F",
-                "D0608H",
-                "D0610A",
-                "D0612C",
-                "D0614E",
-                "D0616G",
-                "D0618I",
-                "D0620B",
-                "D0622D",
-                "D0624F",
-                "D0626H",
-                "D0628A",
-                    "D0630C",
-                    "D0632E",
-                    "D0634G",
-                    "D0636I",
-                    "D0638B",
-                    "D0640D",
-                    "D0642F",
-                    "D0644H",
-                    "D0646A",
-                    "D0648C",
-                    "D0650E"]
+            # "D0607G",
+            # "D0609I",
+            # "D0611B",
+            # "D0613D",
+            # "D0615F",
+            # "D0617H",
+            # "D0619A",
+            # "D0621C",
+            # "D0623E",
+            # "D0625G",
+            # "D0627I",
+            # "D0629B",
+            # "D0631D",
+            # "D0633F",
+            # "D0635H",
+            # "D0637A",
+            # "D0639C",
+            # "D0641E",
+            # "D0643G",
+            # "D0645I",
+            "D0647B",
+            "D0649D",
+            "D0602B",
+            "D0604D",
+            "D0606F",
+            "D0608H",
+            "D0610A",
+            "D0612C",
+            "D0614E",
+            "D0616G",
+            "D0618I",
+            "D0620B",
+            "D0622D",
+            "D0624F",
+            "D0626H",
+            "D0628A",
+            "D0630C",
+            "D0632E",
+            "D0634G",
+            "D0636I",
+            "D0638B",
+            "D0640D",
+            "D0642F",
+            "D0644H",
+            "D0646A",
+            "D0648C",
+            "D0650E"]
     else:
         topics = [args[1]]
     for topic in topics:
